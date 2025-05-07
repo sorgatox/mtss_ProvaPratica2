f90e37196dc949f4b573800be7cbb37750153bfd
sorgatox <enricosorgato0310@gmail.com>
2025-05-07 16:56:37 +0200

    Creare il progetto con Maven close #1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# mtss_ProvaPratica2
+
+# MTSS - A.A. 2024/2025
+
+* Enrico Sorgato: 2071112
+* Alessandro Bertolazzi: 1227274
---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////
+// [Enrico] [Sorgato] [2071112]
+// [Alessandro] [Bertolazzi] [1227274]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class IntegerToRoman 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
